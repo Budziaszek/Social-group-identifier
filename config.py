e0a371efaa6b5a7190a2ef3ad0b7c9836f724871
Michael Kasprzyk <michal0kasprzyk@gmail.com>
2020-01-13 11:46:39 +0100

    + Added gitignore file. + Fixed minimal chance of becoming friend. Previous value allowed for too many of them for larger number of users.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # User consts
 INITIAL_RELATION_VALUE = 1
 RELATION_DECAY_PER_CYCLE = 0.9
-MIN_CHANCE_FOR_FRIENDS = 0.15
+MIN_CHANCE_FOR_FRIENDS = 0.015
 
 # Post consts
 TAGS = ["dog", "cat", "lama", "rabbit"]
