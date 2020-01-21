069e3f3f88066f455f06396a516043ab4c4e9bbe
KordianD <KordianD@users.noreply.github.com>
2020-01-21 13:21:42 +0100

    Add showing plot

--- main.py
+++ main.py
@@ -23,9 +23,16 @@ for user in model.users:
     user.present_roles()
 
 biggest_group = model.datacollector.get_model_vars_dataframe()
+plt.title('Biggest group throughout whole program')
 biggest_group.plot()
 
 plt.show()
 
 number_of_post_written = model.datacollector.get_agent_vars_dataframe()
 print(number_of_post_written)
+
+plt.figure()
+plt.title('Number of post written throughout whole program for agent 0')
+one_agent_wealth = number_of_post_written.xs(0, level="AgentID")
+one_agent_wealth.Post_written.plot()
+plt.show()
